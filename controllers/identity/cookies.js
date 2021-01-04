cc57e92fe6b70b0513903518fdca06fffc11dc59
victorocna <victorocnarescu@gmail.com>
2021-01-04 18:13:54 +0200

    update cookie options

--- controllers/identity/cookies.js
+++ controllers/identity/cookies.js
@@ -1,6 +1,9 @@
 module.exports = async (req, res) => {
   // set a temporary test cookie
-  res.cookie('current_timestamp', +Date.now());
+  const options = {
+    sameSite: 'none',
+  };
+  res.cookie('current_timestamp', +Date.now(), options);
 
   return res.status(200).json({ success: true, message: 'Cookie sent' });
 };
