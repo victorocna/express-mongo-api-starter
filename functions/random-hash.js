86c2d006b6ca4769add3e995fff8b6f5b1c5a6b6
victorocna <victorocnarescu@gmail.com>
2021-01-28 21:21:35 +0200

    tweak function

--- functions/random-hash.js
+++ functions/random-hash.js
@@ -1,6 +1,9 @@
-module.exports = () => {
+const randomHash = () => {
+  // prettier-ignore
   return (
     Math.random().toString(36).substring(2, 15) +
     Math.random().toString(36).substring(2, 15)
   );
 };
+
+module.exports = randomHash;
