4334513864d380a178683561da47c3cd5eb1a7b2
victorocna <victorocnarescu@gmail.com>
2021-04-02 15:06:20 +0300

    update read many filter

--- controllers/todo/read-many.js
+++ controllers/todo/read-many.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
     throw error(404, 'Missing required params');
   }
 
-  const filter = {};
+  const filter = { identity: me };
   const { only } = req.query;
   if (only === 'completed') {
     filter.done = true;
