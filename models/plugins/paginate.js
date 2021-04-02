5dd0a06362b57ff217c45d8954abdd5ddd7941cc
unknown <cristi.albescu22@gmail.com>
2021-04-02 14:48:21 +0300

    paginate now works as intended

--- models/plugins/paginate.js
+++ models/plugins/paginate.js
@@ -1,12 +1,24 @@
-const sliceAndDice = require('../../functions/slice-and-dice');
-
 const paginate = async function (options) {
-  const { offset, sort, per_page: perPage = 30, page: currentPage = 1 } = sliceAndDice(options);
+  const {
+    direction = -1,
+    order = 'created_at',
+    per_page: perPage = 30,
+    page: currentPage = 1,
+  } = options;
+
+  if (isNaN(perPage)) {
+    throw new Error('Paginate error: perPage must be a number.');
+  }
+  if (isNaN(currentPage)) {
+    throw new Error('Paginate error: currentPage must be a number.');
+  }
 
   const mQuery = this.find(this._conditions);
-  mQuery.sort(sort);
+  const offset = (currentPage - 1) * perPage;
+
+  mQuery.sort({ [order]: direction });
   mQuery.skip(offset);
-  mQuery.limit(perPage);
+  mQuery.limit(+perPage);
 
   // @see https://mongoosejs.com/docs/tutorials/lean.html
   mQuery.lean(true);
@@ -16,6 +28,7 @@ const paginate = async function (options) {
   mQuery.limit(); // reset limit
   mQuery.skip(); // reset offset
   const count = await mQuery.countDocuments(this._conditions).exec();
+
   const hasNext = count > parseInt(pages.length + offset);
   const pageParams = {
     count: +count,
