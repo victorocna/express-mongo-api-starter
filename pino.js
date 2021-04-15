babf63822dd3cb036ef1fb41fdcba1beec2e80dc
victorocna <victorocnarescu@gmail.com>
2021-04-15 23:41:09 +0300

    add pino logger

---
+++ pino.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const pino = require('pino');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const logEverywhere = process.env.LOG_EVERYWHERE === 'yes';
+
+const logger = pino({
+  enabled: !isProduction || logEverywhere,
+});
+
+module.exports = logger;
+module.exports.logger = logger;
