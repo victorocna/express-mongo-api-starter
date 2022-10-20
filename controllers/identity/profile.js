b47afaaee6f106fd815831b7fde17ea170b9e64c
victorocna <victorocnarescu@gmail.com>
2022-10-20 14:28:47 +0300

    tweak variables

--- controllers/identity/profile.js
+++ controllers/identity/profile.js
@@ -2,15 +2,14 @@ const { error } = require('../../functions');
 const { Identity } = require('../../models');
 
 module.exports = async (req, res) => {
-  const { me } = req.user;
-  if (!me) {
+  if (!req.user?.me) {
     throw error(404, 'Missing required params');
   }
 
-  const profile = await Identity.findById(me).lean();
-  if (!profile) {
-    throw error(404, 'Profile not found');
+  const document = await Identity.findById(req.user.me).lean();
+  if (!document) {
+    throw error(404, 'Account not found');
   }
 
-  return res.status(200).json(profile);
+  return res.status(200).json(document);
 };
