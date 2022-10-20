8ba0fe36cbf0fb7fe68153514f395eafa03834ce
victorocna <victorocnarescu@gmail.com>
2022-10-20 14:28:56 +0300

    add change password logic

---
+++ controllers/identity/change-password.js
@@ -0,0 +1,30 @@
+const { hashSync } = require('bcryptjs');
+const { Identity } = require('../../models');
+const { error } = require('../../functions');
+
+module.exports = async (req, res) => {
+  if (!req.user?.me) {
+    throw error(404, 'Missing required params');
+  }
+
+  const { changePassword, confirmPassword } = req.body;
+  if (!changePassword || !confirmPassword) {
+    throw error(400, 'Missing required params');
+  }
+
+  if (changePassword !== confirmPassword) {
+    throw error(400, 'Passwords do not match');
+  }
+
+  const document = await Identity.findById(req.user.me);
+  if (!document) {
+    throw error(500, 'Account not found');
+  }
+
+  await document.updateOne({ password: hashSync(changePassword) });
+
+  return res.status(200).json({
+    data: document,
+    message: 'Password was changed successfully',
+  });
+};
