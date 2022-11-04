7771fbdbf4863e0cec31fd42cfa717c7efb8cc72
victorocna <victorocnarescu@gmail.com>
2022-11-04 12:56:23 +0200

    add falsy

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const coffee = require('./coffee');
 const connectToMongo = require('./connect');
 const error = require('./error');
+const falsy = require('./falsy');
 const randomHash = require('./random-hash');
 const removeRefreshTokenCookie = require('./remove-refresk-token-cookie');
 
@@ -8,6 +9,7 @@ module.exports = {
   coffee,
   connectToMongo,
   error,
+  falsy,
   randomHash,
   removeRefreshTokenCookie,
 };
