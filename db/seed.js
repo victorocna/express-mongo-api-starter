ace04e21e0609fe80e1df5ad531d48a371de61f7
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2023-07-13 15:29:41 +0300

    drop db before seeding, unless --keep parameter is specified

--- db/seed.js
+++ db/seed.js
@@ -1,24 +1,46 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
 const connectToMongo = require('../functions/connect');
 const identities = require('./seeds/001_identities');
+
+const dropCollections = async () => {
+  try {
+    const connection = mongoose.connection;
+    const collections = await connection.db.listCollections().toArray();
+
+    for (let collection of collections) {
+      console.log(`Dropping ${collection.name}`);
+      await connection.db.dropCollection(collection.name);
+      console.log('✓');
+    }
+  } catch (err) {
+    console.error(err);
+    throw new Error('Could not clean db.');
+  }
+};
 
 const seed = async (params) => {
   if (!process.env.MONGODB_URI) {
     throw new Error('You must set your environment variables before running this script');
   }
-  if (process.env.MONGODB_URI.includes('mongodb+srv') && params !== '--force') {
-    console.warn('Info: Use `npm run seed -- --force` to run this seed on a live database');
+  if (process.env.MONGODB_URI.includes('mongodb+srv') && !params.includes('--force')) {
+    console.warn('Info: Use `--force` param to run this seed on a live database');
     throw new Error("You can't run this seed on a live database");
   }
 
   await connectToMongo();
+
+  if (!params.includes('--keep')) {
+    await dropCollections();
+  }
 
   await identities.seed();
 };
 
 (async () => {
   try {
-    const params = process.argv;
-    await seed(params[2]); // invoke function
+    const params = process.argv.slice(2);
+    await seed(params); // invoke function
     process.exit();
   } catch (err) {
     console.error(err);
