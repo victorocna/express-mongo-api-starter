b4a4f33f4fc088f3d209f5561b2576ed51f04830
victorocna <victorocnarescu@gmail.com>
2023-07-18 14:24:48 +0300

    update schema

--- examples/schemas/todo-schema.js
+++ examples/schemas/todo-schema.js
@@ -1,6 +1,10 @@
 const yup = require('yup');
 
-module.exports = yup.object().shape({
-  name: yup.string().trim().required(),
-  done: yup.boolean(),
-});
+module.exports = yup
+  .object()
+  .shape({
+    name: yup.string().trim().required(),
+    done: yup.boolean(),
+  })
+  // removes unwanted or unknown fields
+  .noUnknown(true);
