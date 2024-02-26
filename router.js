30ba144a5f9b1c6934c5d5c7b166a7de2cca1bcf
victorocna <victorocnarescu@gmail.com>
2024-02-26 17:47:26 +0200

    update dependencies

--- router.js
+++ router.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const middleware = require('express-goodies/middleware');
-const httpContext = require('express-http-context');
 const routes = require('./routes');
 const exampleRoutes = require('./examples/routes');
 
@@ -8,7 +7,6 @@ const router = Router();
 module.exports = router;
 
 // Use express context
-router.use(httpContext.middleware);
 router.use(middleware.httpContext);
 
 // Use speed limiter for all requests
