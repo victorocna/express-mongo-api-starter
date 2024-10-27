1a9418471ad8fe53147a3489431be5323e9566ce
victorocna <victorocnarescu@gmail.com>
2024-10-27 15:04:43 +0200

    Update scripts

--- scripts/next.js
+++ scripts/next.js
@@ -12,9 +12,10 @@ const getScriptProps = () => {
       return 'nodemon --exec babel-node --presets=@babel/preset-env -- ./server.js';
     case 'seed':
       return 'babel-node ./db/seed.js';
-    default:
-      // Same as "start"
+    case 'start':
       return 'node ./build/server.js';
+    case 'test':
+      return 'mocha ./tests --require @babel/register';
   }
 };
 
@@ -24,9 +25,13 @@ const runScript = (script) => {
 
   // Check if the script is a node script or npx script
   const command = args[0] === 'node' ? 'node' : 'npx';
+  if (command === 'node') {
+    // Remove the "node" command from the arguments
+    args.shift();
+  }
 
   // Run the script with the arguments, excluding the first element
-  spawn(command, args.slice(1), { stdio: 'inherit', shell: true });
+  spawn(command, args, { stdio: 'inherit', shell: true });
 };
 
 // Run the script
