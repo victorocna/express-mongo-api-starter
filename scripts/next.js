04c0353648e70d5427d71934cc0e521eabb58079
victorocna <victorocnarescu@gmail.com>
2024-11-26 14:09:14 +0200

    Add extra args

--- scripts/next.js
+++ scripts/next.js
@@ -4,18 +4,19 @@ const { spawn } = require('child_process');
 const getScriptProps = () => {
   // Get the command line arguments
   const mode = process.argv[2];
+  const extraArgs = process.argv.slice(3).join(' ');
 
   switch (mode) {
     case 'build':
-      return 'babel . --out-dir ./build';
+      return `babel . --out-dir ./build ${extraArgs}`;
     case 'dev':
-      return 'nodemon --exec babel-node --presets=@babel/preset-env -- ./server.js';
+      return `nodemon --exec babel-node --presets=@babel/preset-env -- ./server.js ${extraArgs}`;
     case 'seed':
-      return 'babel-node ./db/seed.js';
+      return `babel-node ./db/seed.js ${extraArgs}`;
     case 'start':
-      return 'node ./build/server.js';
+      return `node ./build/server.js ${extraArgs}`;
     case 'test':
-      return 'mocha ./tests --require @babel/register';
+      return `mocha ./tests --require @babel/register ${extraArgs}`;
   }
 };
 
