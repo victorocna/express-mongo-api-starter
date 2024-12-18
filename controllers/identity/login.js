cc29444d122377f890052393ca0fdd66fa3a8a6d
mario-miclea <miclea.alexandru.mario@gmail.com>
2024-12-18 18:04:02 +0200

    Fix: Adjusted cookie options for Safari compatibility

--- controllers/identity/login.js
+++ controllers/identity/login.js
@@ -50,11 +50,13 @@ export default async (req, res) => {
 
   // Set refresh token as cookie
   const oneDay = 24 * 3600 * 1000;
+  const isProduction = process.env.NODE_ENV === 'production';
+
   res.cookie(process.env.JWT_TOKEN_NAME, refreshToken, {
     domain: process.env.COOKIE_DOMAIN,
     httpOnly: true,
     maxAge: oneDay,
-    secure: true,
+    secure: isProduction,
     signed: true,
     sameSite: 'Lax',
   });
--- functions/remove-refresh-token-cookie.js
+++ functions/remove-refresh-token-cookie.js
@@ -1,9 +1,11 @@
 const removeRefreshTokenCookie = (res) => {
+  const isProduction = process.env.NODE_ENV === 'production';
+
   res.cookie(process.env.JWT_TOKEN_NAME, '', {
     domain: process.env.COOKIE_DOMAIN,
     httpOnly: true,
     maxAge: 0,
-    secure: true,
+    secure: isProduction,
     signed: true,
     sameSite: 'Lax',
   });
