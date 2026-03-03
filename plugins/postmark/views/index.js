aec134c2e36701bae8712bf0a78eaea593ee5165
victorocna <victorocnarescu@gmail.com>
2026-03-03 14:11:01 +0200

    Format code

--- plugins/postmark/views/index.js
+++ plugins/postmark/views/index.js
@@ -1,3 +1,3 @@
+export { default as confirm } from './confirm';
 export { default as contact } from './contact';
 export { default as reset } from './reset';
-export { default as confirm } from './confirm';
